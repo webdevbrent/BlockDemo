c07756634e07ee7f84b90e8493afd0a5d047c6b9
webdevbrent <webdeveloperbrent@gmail.com>
2018-09-18 11:54:14 -0400

    First Commit Message on BlockDemo

---
+++ src/BlockDemo.java
@@ -0,0 +1,15 @@
+public class BlockDemo {
+    public static void main(String args[]){
+        double i, j, d;
+
+        i = 5;
+        j = 10;
+
+        // The target of this if is a block
+        if(i != 0){
+            System.out.println("i does not equal zero");
+            d = j / i;
+            System.out.println(" j / i is " + d);
+        }
+    }
+}
